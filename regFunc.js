7918b317ad8a58812f90b64266e0d3d3b99c6b61
laddered <Z31bw8uG@yandex.ru>
2018-07-12 09:25:10 +0300

    another

--- regFunc.js
+++ regFunc.js
@@ -2,28 +2,52 @@
 //////////////////start//////////////////
 
 signUpBtn = ()=>{
-    localStorage.setItem('login', VT.getEl('#form_login').value);
-    localStorage.setItem('password', VT.getEl('#form_password').value);
-    localStorage.setItem('email', VT.getEl('#form_email').value);
-    localStorage.setItem('age', VT.getEl('#form_age').value);
-    localStorage.setItem('name', VT.getEl('#form_MyName').value);
+    VT.getEl('#form_login');
+
+
+
+
+
+
+
+    if (JSON.parse(localStorage.getItem("users")) === null) {localStorage.setItem("users", "[]")}
+    let usersArray = JSON.parse(localStorage.getItem("users"));
+    let interimObj = {};
+    getUserInfoFromForm(interimObj, ['login','password','email','age','userName']);
+    interimObj.password = interimObj.password.hashCode();
+    usersArray.push(interimObj);
+    let usersString = JSON.stringify(usersArray);
+    localStorage.setItem("users", usersString);
+};
+
+
+String.prototype.hashCode = function() {
+    for(var ret = 0, i = 0, len = this.length; i < len; i++) {
+        ret = (31 * ret + this.charCodeAt(i)) << 0;
+    }
+    return ret;
+};
+
+getUserInfoFromForm = (interimObj, fields) => {
+    for ( let i = 0; i < fields.length; i++ ) {
+        interimObj[fields[i]] = VT.getValue('#form_' + fields[i]);
+    }
 };
 
 passwordCheck = ()=>{
     let line1 = VT.getValue('#form_password');
     let line2 = VT.getValue('#form_confirmPassword');
     if ( line1 !== line2 ) {
-        VT.addClass('#buttonSignUp', 'disabled');
-        VT.getEl('#form_password').classList.remove("styleForGood"); VT.getEl('#form_password').classList.add("styleForBad");
-        VT.getEl('#form_confirmPassword').classList.remove("styleForGood"); VT.getEl('#form_confirmPassword').classList.add("styleForBad");
+        VT.getEl('#buttonSignUp').disabled = true;
+        VT.removeClass('#form_password', 'goodS'); VT.addClass('#form_password', 'badS');
+        VT.removeClass('#form_confirmPassword', 'goodS'); VT.addClass('#form_confirmPassword', 'badS');
     }
     else {
-        VT.removeClass('#buttonSignUp', 'disabled');
-        VT.getEl('#form_password').classList.remove("styleForBad"); VT.getEl('#form_password').classList.add("styleForGood"); setTimeout(function() {VT.getEl('#form_password').classList.remove("styleForGood")}, 3000);
-        VT.getEl('#form_confirmPassword').classList.remove("styleForBad"); VT.getEl('#form_confirmPassword').classList.add("styleForGood"); setTimeout(function() {VT.getEl('#form_confirmPassword').classList.remove("styleForGood")}, 3000);
+        VT.getEl('#buttonSignUp').disabled = false;
+        VT.removeClass('#form_password', 'badS'); VT.addClass('#form_password', 'goodS'); setTimeout(function() {VT.removeClass('#form_password', 'goodS')}, 3000);
+        VT.removeClass('#form_confirmPassword', 'badS'); VT.addClass('#form_confirmPassword', 'goodS'); setTimeout(function() {VT.removeClass('#form_confirmPassword', 'goodS')}, 3000);
     }
 };
-
 
 
 hideClick = ()=>{
@@ -38,10 +62,39 @@ hideClick = ()=>{
     }
 };
 
-ff = ()=>{
-    console.log(VT.getEl('#form_login').value);
-    console.log(VT.getEl('#form_password').value);
-    console.log(VT.getEl('#form_email').value);
-    console.log(VT.getEl('#form_age').value);
-    console.log(VT.getEl('#form_MyName').value);
+loginCheck = ()=>{
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+f = ()=>{
+};
+
+var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+var email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("I expect an e-mail, darling!");
+    } else {
+        email.setCustomValidity("");
+    }
+});
